3156bc0b0e0ab6ffe6bbac58d6a8f77b79ae80e4
mohdyel <121559522+mohdyel@users.noreply.github.com>
2022-12-28 16:34:23 +0300

    Create a.py

---
+++ a.py
@@ -0,0 +1,40 @@
+import zipfile
+import itertools
+def openZip(file, password=''):
+    zip = zipfile.ZipFile(file)
+    try:
+        if password == '':
+            zip.extractall()
+        else:
+            zip.extractall(pwd=bytes(password, 'utf-8'))
+        return True
+    except Exception as e:
+        return False
+#zip file
+file = 'File.zip'
+def foo(l):
+     yield from itertools.product(*([l] * 6)) 
+x = 0
+# number or chars you want to add
+for x in foo('1234567890'):
+    word = (''.join(x))
+    result = openZip(file, word)
+    if result:
+        print('the correct password is',word)
+        break
+    else:
+         print('incorrect:',word)
+     
+def openZip(file, password=''):
+    zip = zipfile.ZipFile(file)
+    try:
+        if password == '':
+            zip.extractall()
+        else:
+            zip.extractall(pwd=bytes(word, 'utf-8'))
+        return True
+    except Exception as e:
+        return False
+
+
+
